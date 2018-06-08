c1193c8bcba07b2ba43b8289e4971d532b9c1a28
Your Name <you@example.com>
2018-06-08 19:12:41 -0300

    .

--- src/components/playersGrid/PlayersGrid.jsx
+++ src/components/playersGrid/PlayersGrid.jsx
@@ -1,44 +1,58 @@
 import React, { Component } from "react";
-import { Container, ListGroup, Alert } from "reactstrap";
+import { Container, ListGroup, Alert, ListGroupItem } from "reactstrap";
 import { Row, Col } from "reactstrap";
 import "./PlayersGrid.css";
 
 const PlayersGrid = ({ rows }) => {
-  if (rows.length) {
-    return (
-      <Container className="players-container">
-        <Row />
-        <Container>
-          <ListGroup>
-            <Row>
-              <Col xs="3">
-                <span>
-                  <strong>Player</strong>
-                </span>
-              </Col>
-              <Col xs="3">
-                <span>
-                  <strong>Position</strong>
-                </span>
-              </Col>
-              <Col xs="3">
-                <span>
-                  <strong>Team</strong>
-                </span>
-              </Col>
-              <Col xs="3">
-                <span>
-                  <strong>Age</strong>
-                </span>
-              </Col>
-            </Row>
-          </ListGroup>
-        </Container>
+  return (
+    <Container className="players-container">
+      <Row />
+      <Container>
+        <ListGroup>
+          <Row>
+            <Col xs="3">
+              <span>
+                <strong>Player</strong>
+              </span>
+            </Col>
+            <Col xs="3">
+              <span>
+                <strong>Position</strong>
+              </span>
+            </Col>
+            <Col xs="3">
+              <span>
+                <strong>Team</strong>
+              </span>
+            </Col>
+            <Col xs="3">
+              <span>
+                <strong>Age</strong>
+              </span>
+            </Col>
+          </Row>
+          <Row>
+            <ListGroupItem>
+              {rows.length > 0 ? (
+                rows.map((player, i) => (
+                  <Row>
+                    <Col xs="6">
+                      <span>{player.name}</span>
+                    </Col>
+                    <Col xs="6">
+                      <span>{player.position}</span>
+                    </Col>
+                  </Row>
+                ))
+              ) : (
+                <Alert color="warning">Any players yet</Alert>
+              )}
+            </ListGroupItem>
+          </Row>
+        </ListGroup>
       </Container>
-    );
-  } else {
-    return <Alert color="warning">Any Players yet.</Alert>;
-  }
+    </Container>
+  );
 };
 
 export default PlayersGrid;
