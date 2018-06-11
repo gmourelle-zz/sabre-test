71395d8c7e1a36db3f37f7e8c5a56b20290d13fa
Your Name <you@example.com>
2018-06-11 11:50:44 -0300

    .

--- src/components/playersGrid/PlayersGrid.jsx
+++ src/components/playersGrid/PlayersGrid.jsx
@@ -6,51 +6,56 @@ import "./PlayersGrid.css";
 const PlayersGrid = ({ rows }) => {
   return (
     <Container className="players-container">
-      <Row />
-      <Container>
-        <ListGroup>
-          <Row>
-            <Col xs="3">
-              <span>
-                <strong>Player</strong>
-              </span>
-            </Col>
-            <Col xs="3">
-              <span>
-                <strong>Position</strong>
-              </span>
-            </Col>
-            <Col xs="3">
-              <span>
-                <strong>Team</strong>
-              </span>
-            </Col>
-            <Col xs="3">
-              <span>
-                <strong>Age</strong>
-              </span>
-            </Col>
-          </Row>
-          <Row>
+      <ListGroup>
+        <Row>
+          <Col xs="3">
+            <span>
+              <strong>Player</strong>
+            </span>
+          </Col>
+          <Col xs="3">
+            <span>
+              <strong>Position</strong>
+            </span>
+          </Col>
+          <Col xs="3">
+            <span>
+              <strong>Team</strong>
+            </span>
+          </Col>
+          <Col xs="3">
+            <span>
+              <strong>Age</strong>
+            </span>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12">
             <ListGroupItem>
               {rows.length > 0 ? (
                 rows.map((player, i) => (
                   <Row>
-                    <Col xs="6">
+                    <Col xs="3">
                       <span>{player.name}</span>
                     </Col>
-                    <Col xs="6">
+                    <Col xs="3">
                       <span>{player.position}</span>
+                    </Col>
+                    <Col xs="3">
+                      <span>{player.nationality}</span>
+                    </Col>
+                    <Col xs="3">
+                      <span>{player.dateOfBirth}</span>
                     </Col>
                   </Row>
                 ))
               ) : (
-                  <Alert color="warning">Any players yet</Alert>
-                )}
+                <Alert color="warning">Any players yet</Alert>
+              )}
             </ListGroupItem>
-          </Row>
-        </ListGroup>
-      </Container>
+          </Col>
+        </Row>
+      </ListGroup>
     </Container>
   );
 };
